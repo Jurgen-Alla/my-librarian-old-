e5f7f4b6cf9335110c39ae57b2609ac19193e8bc
Jurgen Alla <jurgenalla98@gmail.com>
2023-08-15 23:15:51 +0200

    Added welcome page fixed window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:my_librarian/app.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
+  await getAppWindow();
   runApp(const App());
+}
+
+Future<void> ensureInitialized() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+}
+
+Future<void> getAppWindow() async {
+  await initializeWindow();
+  await startWindow();
+}
+
+Future<void> initializeSizeFeatures() async {
+  await windowManager.setResizable(false);
+  await windowManager.setMaximizable(false);
+}
+
+Future<void> initializeWindow() async {
+  await ensureInitialized();
+  await initializeSizeFeatures();
+}
+
+Future<void> startWindow() async {
+  const WindowOptions windowOptions = WindowOptions(
+    size: Size(400, 600),
+    center: true,
+  );
+  await windowManager.waitUntilReadyToShow(windowOptions, whenReadyToShow);
+}
+
+Future<void> whenReadyToShow() async {
+  await windowManager.show();
+  await windowManager.focus();
 }
